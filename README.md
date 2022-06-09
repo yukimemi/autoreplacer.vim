128bc0b21e88203f044089c7f1ebc749024fc41a
yukimemi <yukimemi@gmail.com>
2022-06-09 17:52:45 +0900

    :zap: Update config and readme.

--- README.md
+++ README.md
@@ -2,24 +2,98 @@
 
 ## Auto date writing.
 
-### require.
+- Before.
+
+```markdown
+Last Change: .
+```
+
+- After save.
+
+```markdown
+Last Change: 2022/06/09 16:59:43.
+```
+
+### Require.
 
 -   [Deno - A modern runtime for JavaScript and TypeScript](https://deno.land/)
 -   [vim-denops/denops.vim: 🐜 An ecosystem of Vim/Neovim which allows developers to write cross-platform plugins in Deno](https://github.com/vim-denops/denops.vim)
 
-## Sample config.
+## Config.
+
+No settings are required. However, the following settings can be made if necessary.
 
 ```vim
 " This is the default setting.
+let g:autodate_debug = v:false
 let g:autodate_config = {
 \ "*": {
-\   "keywords": [
-\     "Last Change.*: (?<date>)$",
-\     "Updated: (?<date>)\."
+\   "replace": [
+\     ['/(.*Last Change.*: ).*\.$/i',
+\     '$1${format(now, "yyyy/MM/dd HH:mm:ss")}.']
 \   ],
-\   "events": ["BufWritePre"]
+\   "events": ["FileWritePre", "BufWritePre"],
+\   "pat": "*",
+\   "head": 13,
+\   "tail": 13,
 \ }
 \ }
+```
+
+### Config description.
+
+```vim
+let g:autodate_config = {
+\ "filetype": {                            # vim's filetype.
+\   "replace": [['before', 'after'], ...], # replace patterns. -> Evaluate deno code: line.replace(before, after)
+                                           # You can use the following values in after.
+                                           #   now   : new Date()
+                                           #   format: Deno format function (https://deno.land/std/datetime#format)
+\   "events": "event",                     # Vim's autocmd events. String or List.
+\   "pat": "aupat",                        # Vim's autocmd pattern. String or List.
+\   "head": 13,                            # The number of lines searched from top.
+\   "tail": 13,                            # The number of lines searched from bottom.
+\ },
+\ "filetype": ...
+\ }
+```
+
+### Example.
+
+```vim
+let g:autodate_config = {
+\ "xml": {
+\   "replace": [
+\     ['/^(.*key="version">)[^<]*(<.*)/i', '$1${format(now, "yyyyMMdd_HHmmss")}$2']
+\   ],
+\   "event": ["BufWritePre"],
+\   "pat": ["*.xml", "*.xaml"],
+\   "head": 30,
+\   "tail": 5,
+\ }
+\ }
+```
+
+- Before.
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<root>
+	<func name="Set-Store">
+		<set key="VERSION"></set>
+	</func>
+</root>
+```
+
+- After save.
+
+```xml
+<?xml version="1.0" encoding="utf-8"?>
+<root>
+	<func name="Set-Store">
+		<set key="VERSION">20220609_165708</set>
+	</func>
+</root>
 ```
 
 ## Commands.
--- denops/autodate/main.ts
+++ denops/autodate/main.ts
@@ -4,42 +4,41 @@ import * as helper from "https://deno.land/x/denops_std@v3.3.1/helper/mod.ts";
 import * as op from "https://deno.land/x/denops_std@v3.3.1/option/mod.ts";
 import * as vars from "https://deno.land/x/denops_std@v3.3.1/variable/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v3.3.1/mod.ts";
-import { Lock } from "https://deno.land/x/async@v1.1.5/mod.ts";
+import { assertBoolean } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 import { batch } from "https://deno.land/x/denops_std@v3.3.1/batch/mod.ts";
 import { format } from "https://deno.land/std@0.141.0/datetime/mod.ts";
 import { merge } from "https://cdn.skypack.dev/lodash@4.17.21";
-import { assertBoolean } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 
 type Config = {
   [key: string]: {
-    keywords: [string, string][];
-    events: autocmd.AutocmdEvent[];
+    replace: [string, string][];
+    event: autocmd.AutocmdEvent | autocmd.AutocmdEvent[];
+    pat: string | string[];
     head: number;
     tail: number;
   };
 };
 
 let now = new Date();
+let nowStr = now.toString();
 let debug = false;
 
 let config: Config = {
   "*": {
-    keywords: [
+    replace: [
       [
         "/(.*Last Change.*: ).*\\.$/i",
         '$1${format(now, "yyyy/MM/dd HH:mm:ss")}.',
       ],
     ],
-    // events: ["BufUnload", "FileWritePre", "BufWritePre"],
-    events: ["BufWritePre"],
-    head: 10,
-    tail: 10,
+    event: ["FileWritePre", "BufWritePre"],
+    pat: "*",
+    head: 13,
+    tail: 13,
   },
 };
 
 let enable = true;
-
-const lock = new Lock();
 
 // deno-lint-ignore no-explicit-any
 const clog = (...data: any[]): void => {
@@ -56,13 +55,13 @@ async function replaceLine(
   index: number,
 ): Promise<void> {
   // deno-lint-ignore require-await
-  return await batch(denops, async () => {
+  await batch(denops, async () => {
     lines.forEach((line, i) => {
       let newLine = line;
       i = index + i;
-      config.keywords.forEach(async (keyword) => {
-        const re = eval(keyword[0]) as RegExp;
-        const after = keyword[1];
+      config.replace.forEach(async (rep) => {
+        const re = eval(rep[0]) as RegExp;
+        const after = rep[1];
         // clog({ i, line, re, after });
         if (re.test(line)) {
           newLine = newLine.replace(re, eval("`" + after + "`"));
@@ -76,7 +75,7 @@ async function replaceLine(
 
 export async function main(denops: Denops): Promise<void> {
   // debug.
-  debug = await vars.g.get(denops, "autodate_debug", false);
+  debug = await vars.g.get(denops, "autodate_debug", debug);
   // Merge user config.
   const userConfig = (await vars.g.get(denops, "autodate_config")) as Config;
   config = merge(config, userConfig);
@@ -85,35 +84,39 @@ export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     async autodate(): Promise<void> {
       try {
-        await lock.with(async () => {
-          if (!enable) {
-            clog(`autodate skip ! enable: [${enable}]`);
+        if (!enable) {
+          clog(`autodate skip ! enable: [${enable}]`);
+          return;
+        }
+        // Get filetype and filetype config.
+        const ft = (await op.filetype.get(denops));
+        clog({ ft });
+
+        const allConfig = config["*"];
+        const ftConfig = config[ft];
+
+        // Update now.
+        now = new Date();
+
+        if (now.toString() === nowStr) {
+          clog(`Same time: [${nowStr}], skip !`);
+          return;
+        }
+        nowStr = now.toString();
+
+        for (const c of [allConfig, ftConfig]) {
+          // clog({ c });
+          if (c == null) {
             return;
           }
-          // Get filetype and filetype config.
-          const ft = (await op.filetype.get(denops));
-          clog({ ft });
+          // Get head and tail.
+          const head = await fn.getline(denops, 1, c.head);
+          const lastline = await fn.line(denops, "$");
+          const tail = await fn.getline(denops, lastline - c.tail, lastline);
 
-          const allConfig = config["*"];
-          const ftConfig = config[ft];
-
-          // Update now.
-          now = new Date();
-
-          [allConfig, ftConfig].forEach(async (c) => {
-            // clog({ c });
-            if (c == null) {
-              return;
-            }
-            // Get head and tail.
-            const head = await fn.getline(denops, 1, c.head);
-            const lastline = await fn.line(denops, "$");
-            const tail = await fn.getline(denops, lastline - c.tail, lastline);
-
-            await replaceLine(denops, c, head, 1);
-            await replaceLine(denops, c, tail, lastline - c.tail);
-          });
-        });
+          await replaceLine(denops, c, head, 1);
+          await replaceLine(denops, c, tail, lastline - c.tail);
+        }
       } catch (e) {
         clog(e);
       }
@@ -145,14 +148,12 @@ export async function main(denops: Denops): Promise<void> {
   await autocmd.group(denops, denops.name, (helper) => {
     helper.remove();
     // Set all filetype autocmd.
-    Object.keys(config).forEach((ft) => {
-      config[ft].events.forEach((e) => {
-        helper.define(
-          e,
-          ft,
-          `call s:${denops.name}_request('autodate', [])`,
-        );
-      });
+    Object.values(config).forEach((c) => {
+      helper.define(
+        c.event,
+        c.pat,
+        `call s:${denops.name}_request('autodate', [])`,
+      );
     });
   });
 
