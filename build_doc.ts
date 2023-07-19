7c64d5a491c3c3517c8eeabb54fe48e5859a95f5
yukimemi <yukimemi@gmail.com>
2023-07-19 10:27:36 +0900

    :zap: Add tag to `build_doc.ts`

--- build_doc.ts
+++ build_doc.ts
@@ -1,4 +1,4 @@
-import { pod2markdown, pod2vimdoc } from "https://pax.deno.dev/yukimemi/deno-pod/mod.ts";
+import { pod2markdown, pod2vimdoc } from "https://pax.deno.dev/yukimemi/deno-pod@0.0.2/mod.ts";
 
 const pod = "./pod/autodate.pod";
 const vimdoc = "./doc/autodate.txt";
