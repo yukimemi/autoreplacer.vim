739da652250960a46b93f293d132ea62ec39d097
GitHub <noreply@github.com>
2024-01-25 06:03:53 +0000

    chore(deps): Update deno dependencies

--- denops/autodate/main.ts
+++ denops/autodate/main.ts
@@ -12,7 +12,7 @@ import * as vars from "https://deno.land/x/denops_std@v5.3.0/variable/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v5.3.0/mod.ts";
 import { assert, is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
 import { batch } from "https://deno.land/x/denops_std@v5.3.0/batch/mod.ts";
-import { format } from "https://deno.land/std@0.212.0/datetime/mod.ts";
+import { format } from "https://deno.land/std@0.213.0/datetime/mod.ts";
 import { merge } from "https://cdn.skypack.dev/lodash@4.17.21";
 
 type Config = {
